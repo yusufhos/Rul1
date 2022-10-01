3fd712fdf987873ac3eddd7754da2b3fd634a94d
yusufhos <107295216+yusufhos@users.noreply.github.com>
2022-10-01 19:48:01 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
+│ • tri [089629015724]
+│ • axis  [083834302288]
+│ • Ovo  [089629015724]
 │ • Saweria  [https://saweria.co/arullofc]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/6289629015724
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
